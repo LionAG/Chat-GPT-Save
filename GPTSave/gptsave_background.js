1cc58131a137bd40772eb177516e8640988c9add
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-12-05 11:49:59 +0100

    [Development] Add a context menu save chat option

--- GPTSave/gptsave_background.js
+++ GPTSave/gptsave_background.js
@@ -1,3 +1,22 @@
+const contextMenuItem =
+{
+  id: 'context-menu-save-gpt-chat',
+  title: 'Save GPT Chat',
+  contexts: ['all']
+};
+
+browser.contextMenus.create(contextMenuItem);
+
+browser.contextMenus.onClicked.addListener((info, tab) =>
+{
+  if(info.menuItemId === 'context-menu-save-gpt-chat')
+  {
+    browser.tabs.executeScript({
+      file: "gptsave.js"
+    });
+  }
+});
+
 browser.browserAction.onClicked.addListener((tab) =>
 {
     browser.tabs.executeScript({
